27b6d5dc42c0e016c93b538c21cc79cf8b04d11b
Michael Roberts <maroberts913@gmail.com>
2016-03-30 22:43:45 -0400

    remove some console logs

--- public/javascripts/controllers/project-controller.js
+++ public/javascripts/controllers/project-controller.js
@@ -7,12 +7,10 @@ projectApp.controller('projectController', function($scope, $http){
 
   $http.get('https://api.github.com/users/MichaelARoberts/repos').then(function(reposResponse){
     $scope.repos = reposResponse.data
-    console.log($scope.repos)
-    
+
     for (repo of $scope.repos){
       $http.get('https://api.github.com/repos/MichaelARoberts/'+ repo.name +'/languages').then(function(langsResponse){
         $scope.languages.push(langsResponse.data)
-        console.log($scope.languages)
       })
     }
 
