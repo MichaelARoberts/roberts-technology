d728a63d6330c640ad37fc62760cfc7a37aa141d
Michael Roberts <maroberts913@gmail.com>
2016-04-02 18:45:08 -0400

    added index page and started about page

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+router.get('/skills', function(req, res, next) {
+  res.render('skills', { title: 'Skills' });
 });
 
 router.get('/resources', function(req, res, next) {
